102616283645e1e9afdeb2920d37c0a1c20982fb
CarlosEReis <carlos.er7@gmail.com>
2023-04-30 17:16:58 -0300

    14.11 - Desafio: criando o Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,20 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from './app.component';
 
-import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { CoreModule } from './core/core.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
 
+    CoreModule,
     LancamentosModule,
     PessoasModule
   ],
