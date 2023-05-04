420a75c812158faf3900cbe9ef145b7384913d03
CarlosEReis <carlos.er7@gmail.com>
2023-05-04 09:34:41 -0300

    17.14 - Desafio: mensagem de erro de usuário na exclusão de pessoa

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MessageService } from 'primeng/api';
 
@@ -13,6 +14,12 @@ export class ErrorHandlerService {
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse
+    } else if (errorResponse instanceof HttpErrorResponse && errorResponse.status >= 400 && errorResponse.status <= 499) {
+      msg = 'Ocorreu um erro ao processar a sua requisição.';
+      try {
+        msg = errorResponse.error[0].mensagemUsuario;
+      } catch (error) { }
+      console.log('Ocorreu um erro: ', errorResponse);
     } else {
       msg = 'Erro ao processar serviço remoto. Tente novamente, ou chame o administrador do sistema.'
       console.log('Ocorreu um erro: ', errorResponse);
