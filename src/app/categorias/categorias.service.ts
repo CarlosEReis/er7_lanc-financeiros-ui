1647e11299945220b2f09d7776043bc3990ff712
CarlosEReis <carlos.er7@gmail.com>
2023-05-04 13:59:37 -0300

    17.16 - Desafio: implementando o serviço de listagem de categorias

---
+++ src/app/categorias/categorias.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriasService {
+
+  categoriasUrl = 'http://localhost:8080/categorias';
+
+  constructor(private http: HttpClient) { }
+
+  listarTodas(): Promise<any> {
+    const headers = new HttpHeaders()
+    .append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    return this.http.get(this.categoriasUrl, { headers })
+    .toPromise();
+  }
+}
