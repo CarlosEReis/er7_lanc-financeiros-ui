a18d1a850489f50d79e394345907b5492bff62f6
CarlosEReis <carlos.er7@gmail.com>
2023-05-16 13:32:32 -0300

    19.08 - Exibindo o nome do usuário logado

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/seguranca/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   exibindoMenu = false;
+  usuarioLogado =  '';
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.usuarioLogado = this.authService.jwtPayload?.nome;
   }
 
 }
